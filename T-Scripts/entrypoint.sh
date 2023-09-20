0bd933b1bce4bac9b50baff4f2aef75501da4135
jaiswaladi2468 <35973878+jaiswaladi2468@users.noreply.github.com>
2023-09-20 22:27:09 +0530

    Update entrypoint.sh

--- T-Scripts/entrypoint.sh
+++ T-Scripts/entrypoint.sh
@@ -4,8 +4,7 @@ sudo apt install -y docker.io
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
-sudo apt install maven -y
 git clone https://github.com/jaiswaladi246/jpetstore-6.git
-cd jpetstore-6.git
+cd jpetstore-6
 sudo docker build -t petstore .
 sudo docker run -d -p 8080:8080 petstore
