24a161c5f222fecd93e49edd8397720f1af0efa4
jaiswaladi2468 <35973878+jaiswaladi2468@users.noreply.github.com>
2023-09-20 21:28:15 +0530

    Update entrypoint.sh

--- T-Scripts/entrypoint.sh
+++ T-Scripts/entrypoint.sh
@@ -4,4 +4,8 @@ sudo apt install -y docker.io
 sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
-sudo docker run -d -p 8080:80 nginx
+sudo apt install maven -y
+git clone https://github.com/jaiswaladi246/Devops-CICD.git
+cd Devops-CICD
+sudo docker build -t abc .
+sudo docker run -d -p 8080:8080 abc
