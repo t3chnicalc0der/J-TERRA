06b3190c444a8214fd896582ad4a2bca95ed1909
jaiswaladi2468 <35973878+jaiswaladi2468@users.noreply.github.com>
2023-09-20 22:18:36 +0530

    Update entrypoint.sh

--- T-Scripts/entrypoint.sh
+++ T-Scripts/entrypoint.sh
@@ -5,7 +5,7 @@ sudo systemctl start docker
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
 sudo apt install maven -y
-git clone https://github.com/jaiswaladi246/Devops-CICD.git
+git clone https://github.com/jaiswaladi246/jpetstore-6.git
 cd Devops-CICD
 mvn package
 sudo docker build -t abc .
