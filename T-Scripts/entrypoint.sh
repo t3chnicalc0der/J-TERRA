cf98b68d7876119ae0efb2e6e5604e23f55ef98c
jaiswaladi2468 <35973878+jaiswaladi2468@users.noreply.github.com>
2023-09-20 22:37:19 +0530

    Update entrypoint.sh

--- T-Scripts/entrypoint.sh
+++ T-Scripts/entrypoint.sh
@@ -6,5 +6,5 @@ sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
 git clone https://github.com/jaiswaladi246/jpetstore-6.git
 cd jpetstore-6
-sudo docker build -t petstore .
+sudo docker build petstore .
 sudo docker run -d -p 8080:8080 petstore
